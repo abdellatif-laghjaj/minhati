3439d2d1c488e6d2898ed9ce1f03b54daa6f620e
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-08 14:10:55 +0100

    fix some bugs

--- js/app.js
+++ js/app.js
@@ -18,7 +18,6 @@ createApp({
         },
         changeTheme() {
             const theme_items = document.querySelectorAll('.theme-item');
-
             theme_items.forEach(item => {
                 item.addEventListener('click', () => {
                     //change data theme attribute of html tag
