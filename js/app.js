52f5f5096311379f5f4ba4e38c15c1b5a8d69420
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-08 13:26:38 +0100

    use vue js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,37 @@
-const theme_items = document.querySelectorAll('.theme-item');
-
-document.addEventListener('DOMContentLoaded', loadThemeFromLocalStorage);
-
-
-theme_items.forEach(item => {
-    item.addEventListener('click', () => {
-        //change data theme attribute of html tag
-        document.documentElement.setAttribute('data-theme', item.getAttribute('theme-data'));
-        //save theme to local storage
-        saveThemeToLocalStorage(item.getAttribute('theme-data'));
-    });
-});
-
-
-//save theme to local storage
-function saveThemeToLocalStorage(theme) {
-    localStorage.setItem('theme', theme);
-}
-
-
-//load theme from local storage
-function loadThemeFromLocalStorage() {
-    const theme = localStorage.getItem('theme');
-    if (theme) {
-        document.documentElement.setAttribute('data-theme', theme);
-    }
-}
-
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    }
-  }).mount('#app')
+        return {
+            isLoaded: false,
+        }
+    },
+    methods: {
+        loadThemeFromLocalStorage() {
+            const theme = localStorage.getItem('theme');
+            if (theme) {
+                document.documentElement.setAttribute('data-theme', theme);
+            }
+        },
+        saveThemeToLocalStorage(theme) {
+            localStorage.setItem('theme', theme);
+        },
+        changeTheme() {
+            const theme_items = document.querySelectorAll('.theme-item');
+
+            theme_items.forEach(item => {
+                item.addEventListener('click', () => {
+                    //change data theme attribute of html tag
+                    document.documentElement.setAttribute('data-theme', item.getAttribute('theme-data'));
+                    //save theme to local storage
+                    this.saveThemeToLocalStorage(item.getAttribute('theme-data'));
+                });
+            });
+        }
+    },
+    created() {
+        document.addEventListener('DOMContentLoaded', this.changeTheme);
+        this.loadThemeFromLocalStorage();
+        this.isLoaded = true;
+    },
+}).mount('#app')
